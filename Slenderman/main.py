b51fb6aac1fb0221c2fc33ee138e718a56828770
Natália Rodrigues <natytotherodrigues@gmail.com>
2024-11-09 17:22:17 -0300

    basic intro screen

--- Slenderman/main.py
+++ Slenderman/main.py
@@ -13,6 +13,9 @@ class Game:
         self.character_spritesheet = Spritesheet ('img/character.png')
         self.terrain_spritesheet = Spritesheet ('img/terrain.png')
         self.enemy_spritesheet = Spritesheet ('img/enemy.png')
+        self.font = pygame.font.Font('Slender.ttf', 32)
+
+        self.intro_background = pygame.image.load('./img/introbackground.png')
 
     def createTilemap(self):
         for i, row in enumerate(tilemap):
@@ -61,7 +64,30 @@ class Game:
         pass
 
     def intro_screen(self):
-        pass
+        intro = True
+
+        title = self.font.render('SLENDERMAN', True, WHITE)
+        title_rect = title.get_rect(x=10, y=10)
+        play_button = Button (10, 50, 100, 50, WHITE, BLACK, 'Play', 32)
+
+        while intro:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    intro = False
+                    self.running = False
+            
+            mouse_pos = pygame.mouse.get_pos()
+            mouse_pressed = pygame.mouse.get_pressed()
+
+            if play_button.is_pressed(mouse_pos, mouse_pressed):
+                intro = False
+
+            self.screen.blit(self.intro_background, (0,0))
+            self.screen.blit(title, title_rect)
+            self.screen.blit(play_button.image, play_button.rect)
+            self.clock.tick(FPS)
+            pygame.display.update()
+
 
 g = Game()
 g.intro_screen()
